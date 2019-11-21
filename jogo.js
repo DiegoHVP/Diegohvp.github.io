c7ae0008bc0e5637de51aa0516cd54074e474718
DiegoHVP <53943354+DiegoHVP@users.noreply.github.com>
2019-11-21 08:44:16 -0500

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -159,9 +159,8 @@ textSize(28);
 function menu() {
   //RESTAURAR DADOS
   if(q1!=0)
-    q1 = 0, q1 = 0, x = 30, y = 450, XP = 12000
-  sons[0].setVolume(0,1);
-  sons[0].play();
+    q1 = 0, q1 = 0, x = 30, y = 450, XP = 12000, fase = 1
+
   //TELA
   L++
   S++
@@ -196,7 +195,8 @@ function menu() {
   strokeWeight(5)
   stroke(255, 0, 0);
   rect(xo, yo, 130, 65, 30);
-
+  sons[0].setVolume(0,1);
+  sons[0].play();
 
 }
 
